50249575c2526fe84e0f078d6445055d39378354
Diky-Without-C <snayzher@gmail.com>
2024-06-21 17:39:01 +0700

    fix: use click to open card

--- src/components/BirthdayCard/index.tsx
+++ src/components/BirthdayCard/index.tsx
@@ -1,18 +1,25 @@
+import { useState } from "react";
 import useGlobalContext from "@/context/globalVariable";
 import FrontSection from "./Front";
 import InsideSection from "./Inside";
 import "@assets/style/birthdayCard.css";
 
 export default function BirthdayCard() {
+  const [isClick, setIsClick] = useState(false);
   const { data } = useGlobalContext();
   const { isBoxOpen } = data;
 
+  function handleClick() {
+    setIsClick((prev) => !prev);
+  }
+
   return (
     <section
+      onClick={handleClick}
       className={`${isBoxOpen ? "popUp block" : "hidden"} absolute z-50 origin-center`}
     >
       <section className="birthdayCard">
-        <FrontSection />
+        <FrontSection isOpen={isClick} />
         <InsideSection />
       </section>
     </section>
